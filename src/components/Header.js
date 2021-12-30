294d99a79f253e82c5fb25eef22091e5d2036bd0
santiago clemenzi <clemenzisantiago@gmail.com>
2021-12-30 12:51:55 -0300

    header_creado

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+// importamos el Font awsome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// importamos el icono
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+// creamos componente
+const Header = () => {
+	return (
+		<header className="header">
+			<h1 className="header__tittle">Lista de tareas</h1>
+			<button className="header__boton">
+				ocultar tareas hechas <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton'/>
+			</button>
+		</header>
+	);
+};
+
+export default Header;
